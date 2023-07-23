f4e3ac598dbbe35de409af211bc593993e6d5926
adginr <nickolai.zt@gmail.com>
2023-07-23 14:16:07 +0300

    added new attr for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,21 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Git
     abbr --add gc git checkout
     abbr --add gcb git checkout -b 
     abbr --add gac 'git add . && git commit -m'
-    abbr --add pr poetry run 
     abbr --add gs git status
+    abbr --add g git status
+    abbr --add push git push
+    abbr --add pull git pull
     abbr --add gpo git push origin
     abbr --add gp git push
-    abbr --add pull git pull
     abbr --add master git checkout master
+    # Poetry
+    abbr --add pr poetry run 
+    # Poetry Project Specific
     abbr --add prt poetry run ./test.sh
     abbr --add pra poetry run ./analysis.sh
+    # Docker
     abbr --add up docker-compose up
     abbr --add build docker-compose build
 end
